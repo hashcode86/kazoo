527f66e172646fe478291dadeea4a2ecc4a20ab3
DESKTOP-5NK11S6\thang <dothangbk49@gmail.com>
2020-03-06 18:01:43 +0700

    test

--- core/kazoo_speech/src/tts/kazoo_tts_google.hrl
+++ core/kazoo_speech/src/tts/kazoo_tts_google.hrl
@@ -119,6 +119,8 @@
         ,#voiceDesc{name = <<"fr-FR-Standard-D">>, ssmlGender = <<"MALE">>, naturalSampleRateHertz = 24000, languageCodes = [<<"fr-FR">>]}
         ,#voiceDesc{name = <<"vi-VN-Standard-A">>, ssmlGender = <<"FEMALE">>, naturalSampleRateHertz = 24000, languageCodes = [<<"vi-VN">>]}
         ,#voiceDesc{name = <<"vi-VN-Standard-B">>, ssmlGender = <<"MALE">>, naturalSampleRateHertz = 24000, languageCodes = [<<"vi-VN">>]}
+        ,#voiceDesc{name = <<"id-ID-Standard-A">>, ssmlGender = <<"FEMALE">>, naturalSampleRateHertz = 24000, languageCodes = [<<"id-ID">>]}
+        ,#voiceDesc{name = <<"id-ID-Standard-B">>, ssmlGender = <<"MALE">>, naturalSampleRateHertz = 24000, languageCodes = [<<"id-ID">>]}
         ]
        ).
 
